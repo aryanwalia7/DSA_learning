7874e9ed2e80a27fd76e6e31c14cff1d010686cc
aryanwalia7 <aryanwalia739@gmail.com>
2026-07-29 13:23:56 +0530

    Time: 11 ms (26.72%), Space: 48.1 MB (40.46%) - LeetHub

--- 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.java
+++ 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.java
@@ -1,24 +1,23 @@
 class Solution {
     public String maximumNumber(String num, int[] change) {
-        StringBuilder sb= new StringBuilder(num);
-        boolean si=false;
+        StringBuilder sb=new StringBuilder(num);
+        boolean s=false;
         for(int i=0;i<num.length();i++){
-            int s=num.charAt(i)-'0';
-            if(!si){
-                if(s<change[s]){
-                    si=true;
-                    s=change[s];
-                    sb.setCharAt(i,(char)(s+'0'));
+            int c=num.charAt(i)-'0';
+            if(!s){
+                if(c<change[c]){
+                    s=true;
+                    c=change[c];
+                    sb.setCharAt(i,(char)(c+'0'));
                 }
-            }    
-                else{
-                    if(change[s]>=s){
-                        s=change[s];
-                        sb.setCharAt(i,(char)(s+'0'));
-                    }else{
-                        break;
-                    }
+            }else{
+                if(change[c]>=c){
+                    c=change[c];
+                    sb.setCharAt(i,(char)(c+'0'));
+                }else{
+                    break;
                 }
+            }
         }
         return sb.toString();
     }
