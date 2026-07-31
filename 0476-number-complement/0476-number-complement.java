1d7da68f6a7195d9205088e0b5f3737a471aebcf
aryanwalia7 <aryanwalia739@gmail.com>
2026-07-31 11:48:49 +0530

    Time: 1 ms (22.14%), Space: 42.2 MB (58.92%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findComplement(int num) {
         StringBuilder sb= new StringBuilder();
-        int n1=Integer.numberOfLeadingZeros(num);
         String n=Integer.toBinaryString(num);
         for(int i=0;i<n.length();i++){
             char c=n.charAt(i);
