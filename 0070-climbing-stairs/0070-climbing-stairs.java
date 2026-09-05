cc4744db960dcd8d9fea826d7ead76c87bd76471
aryanwalia7 <aryanwalia739@gmail.com>
2026-09-05 12:07:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,32 @@
+// class Solution {
+
+//     public int climb(int n,int arr[]){
+//         if(n==0) return 1;
+//         if(n<0) return 0;
+//         if(arr[n]!=0){
+//             return arr[n];
+//         }
+
+//         arr[n]=climb(n-1,arr)+climb(n-2,arr);
+//         return arr[n];
+//     }
+//     public int climbStairs(int n) {
+//        int arr[]=new int[n+1];
+//        int a=climb(n,arr);
+//        return a;
+//     }
+// }
 class Solution {
-
-    public int climb(int n,int arr[]){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(arr[n]!=0){
-            return arr[n];
-        }
-
-        arr[n]=climb(n-1,arr)+climb(n-2,arr);
-        return arr[n];
+    HashMap<Integer,Integer> map= new HashMap<>();
+    public int climb(int i,int n){
+       if(i==n)return 1;
+       if(i>n) return 0;
+       if(map.containsKey(i)) return map.get(i);
+       int ans=climb(i+1,n)+climb(i+2,n);
+       map.put(i,ans);
+       return ans;
     }
     public int climbStairs(int n) {
-       int arr[]=new int[n+1];
-       int a=climb(n,arr);
-       return a;
+      return climb(0,n);
     }
 }
